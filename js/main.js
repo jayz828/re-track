7ea24ebcc8968e5a5efdc427c712cfb36c8bae51
Jason Natividad <jainatividad@yahoo.com>
2016-12-11 23:52:07 -0800

    Start working on Start and Stop times in the side bar

--- js/main.js
+++ js/main.js
@@ -1,9 +1,77 @@
 
 
+
+
+addOption();
+
+
+function addOption(){
+
+
+
+	var time = document.getElementById('start-time');
+
+
+	// Use military time as start and end times 
+	var startingTime = 5;
+	var endingTime = 18;
+	var thisTime = 0;
+
+	var allTimes = [];
+
+
+	for (hour = 5; hour <= 18; hour++) { 
+    	// allTimes.push(i);
+
+    	for (min = 0; min < 60; min+=15) { 
+
+    		// if (min == 60 ) {
+    		// 	var minutes = 0;
+    		// 	minutes == "00";
+    		// } else {
+    		// 	minutes == min;
+    		// }
+
+    		var minutes = min;
+
+    		if (minutes == 0) {
+    			console.log(minutes);
+    			minutes = "00";
+    		} 
+
+	    	
+	    	var option = document.createElement('option');
+	    	option.text = hour + ":" + minutes;
+	    	time.add(option);
+
+		}
+
+
+	}
+
+
+
+
+	// for (i = startingTime; i < endingTime; i+100) { 
+    	
+ //    	var option = document.createElement('option');
+	// 	option.text = i;
+	// 	time.add(option);
+
+	// }
+
+}
+
+
+
+
 var addBtn = document.getElementById('add-button');
 
 
 addBtn.addEventListener('click', addBill);
+
+
+
 
 
 
